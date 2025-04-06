5b304698baf1e78c22f2a1abc20da533239e44ab
MariusLarilahti <144674240+MariusLarilahti@users.noreply.github.com>
2025-04-06 17:33:24 +0300

    PlayerSOundsWork

--- Assets/Scripts/Audio/SoundsOnPlayer.cs
+++ Assets/Scripts/Audio/SoundsOnPlayer.cs
@@ -20,6 +20,7 @@ namespace FaS.DiverGame.Audio
         [SerializeField] private AudioClip[] soundList;
         private static SoundsOnPlayer instance;
         private AudioSource audioSource;
+        public AudioSource AudioSource_2;
 
         private void Awake()
         {
@@ -28,20 +29,27 @@ namespace FaS.DiverGame.Audio
         void Start()
         {
             audioSource = GetComponent<AudioSource>();
+            PlaySoundEffect(SoundType.SPLASH, 1.5f);
+            PlayLoop1(SoundType.WAVES, 1.5f);
+            PlayLoop2(SoundType.RAIN, 1);
         }
 
-        public static void PlaySoundEffect(SoundType type, int vol)
+        public static void PlaySoundEffect(SoundType type, float vol)
         {
             float pitchVar = Random.Range(0.95f, 1.05f);
             instance.audioSource.pitch = pitchVar;
             instance.audioSource.PlayOneShot(instance.soundList[(int) type], vol);
-            Debug.Log("Splash");
         }
 
-        public static void PlayLoop(SoundType type, int vol)
+        public static void PlayLoop1(SoundType type, float vol)
         {
             instance.audioSource.loop = true;
             instance.audioSource.PlayOneShot(instance.soundList[(int)type], vol);
+        }
+        public static void PlayLoop2(SoundType type, float vol)
+        {
+            instance.AudioSource_2.loop = true;
+            instance.AudioSource_2.PlayOneShot(instance.soundList[(int)type], vol);
         }
     }
 }
